c58b63a9884fdca499706dae8356c0ff1c53a253
“Daniel Ayala“ <dmayala001@gmail.com>
2016-09-18 19:55:00 -0700

    Refactor towards an object oriented design.

--- GigHub/GigHub/Models/Notification.cs
+++ GigHub/GigHub/Models/Notification.cs
@@ -5,13 +5,47 @@ namespace GigHub.Models
 {
     public class Notification
     {
-        public int Id { get; set; }
-        public DateTime DateTime { get; set; }
-        public NotificationType Type { get; set; }
-        public DateTime? OriginalDateTime { get; set; }
-        public string OriginalVenue { get; set; }
+        public int Id { get; private set; }
+        public DateTime DateTime { get; private set; }
+        public NotificationType Type { get; private set; }
+        public DateTime? OriginalDateTime { get; private set; }
+        public string OriginalVenue { get; private set; }
 
         [Required]
         public Gig Gig { get; set; }
+
+        protected Notification()
+        {
+            
+        }
+
+        private Notification(NotificationType type, Gig gig)
+        {
+            if (gig == null)
+                throw new ArgumentNullException("gig");
+
+            Type = type;
+            Gig = gig;
+            DateTime = DateTime.Now;
+        }
+
+        public static Notification GigCreated(Gig gig)
+        {
+            return new Notification(NotificationType.GigCreated, gig);   
+        }
+
+        public static Notification GigUpdated(Gig newGig, DateTime originalDateTime, string originalVenue)
+        {
+            return new Notification(NotificationType.GigUpdated, newGig)
+            {
+                OriginalDateTime = originalDateTime,
+                OriginalVenue = originalVenue
+            };
+        }
+
+        public static Notification GigCancelled(Gig gig)
+        {
+            return new Notification(NotificationType.GigCanceled, gig);
+        }
     }
 }
